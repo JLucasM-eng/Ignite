48a187b56c0f2206894d382ce6524f4abee3947e
JLucasM-eng <jlm16051997@gmail.com>
2021-08-29 19:15:45 -0300

    :sparkles: applying typescript

--- React/01-github-explorer/src/components/RepositoryList.tsx
+++ React/01-github-explorer/src/components/RepositoryList.tsx
@@ -1,9 +1,15 @@
 import { useEffect, useState } from "react"
 import { RepositoryItem } from "./RepositoryItem"
 
+interface Repository {
+    name:"string"
+    description:"string",
+    html_url:"string"
+}
+
 export function RepositoryList(){
     
-    const [repositories,setRepositories]=useState([])
+    const [repositories,setRepositories]=useState<Repository[]>([])
 
     useEffect(()=>{
         fetch("https://api.github.com/users/adriacaroba/repos")
@@ -17,7 +23,7 @@ export function RepositoryList(){
             <h1>Lista de Repositórios</h1>
             <ul>
                 {repositories.map((repository)=>{
-                    return <RepositoryItem key={repository.id} repository={repository}/>
+                    return <RepositoryItem key={repository.name} repository={repository}/>
                 
                 })}
                 
